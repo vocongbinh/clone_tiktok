a8b10aef4326058e8ed18becf6127d846c3dde0a
vocongbinh <105732042+vocongbinh@users.noreply.github.com>
2023-08-14 13:06:11 +0700

    ui header 1

--- src/components/layout/components/Header/index.js
+++ src/components/layout/components/Header/index.js
@@ -1,5 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Header.module.scss';
+import clsx from 'clsx';
+import { faMagnifyingGlass, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
 function Header() {
-    return <h1> Header</h1>;
+    const cx = clsx.bind(styles);
+
+    return (
+        <div className={clsx(styles.wrapper)}>
+            <h1> tiktok</h1>
+            <div className={clsx(styles.search)}>
+                <input className={clsx(styles.searchInput)} placeholder="Search" />
+                <button className={clsx(styles.clearBtn)}>
+                    <FontAwesomeIcon icon={faXmarkCircle} />
+                </button>
+
+                <button className={clsx(styles.searchBtn)}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={clsx(styles.action)}></div>
+        </div>
+    );
 }
 
 export default Header;
